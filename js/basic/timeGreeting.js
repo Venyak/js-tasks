31dd4136d22739a3fe49cfb90db5ac6e6fa260b0
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-08 12:52:44 +0500

    solved new tasks

---
+++ js/basic/timeGreeting.js
@@ -0,0 +1,13 @@
+const timeGreeting = (time, name) => {
+  if (time >= 6 && time < 12) {
+    return `Доброе утро, ${name}!`;
+  } else if (time >= 12 && time < 18) {
+    return `Добрый день, ${name}!`;
+  } else if (time >= 18 && time < 24) {
+    return `Добрый вечер, ${name}!`;
+  } else {
+    return `Доброй ночи, ${name}!`;
+  }
+};
+
+console.log(timeGreeting(4, 'Ivan'));
