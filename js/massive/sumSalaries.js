67191f9cbd66153ac57d11988f490a23e7ccdfdf
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-12 21:36:28 +0500

    Create sumSalaries.js

---
+++ js/massive/sumSalaries.js
@@ -0,0 +1,17 @@
+/*
+	Напишите код для суммирования всех зарплат и сохраните результат в переменной sum. 
+*/
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+};
+
+let sum = 0;
+
+for (let key in salaries) {
+  sum += salaries[key];
+}
+
+console.log(sum);
