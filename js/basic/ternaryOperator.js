dc14c8c1fefddd6b02bf4e55fe55877c289d2169
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-16 17:01:13 +0500

    solved task

---
+++ js/basic/ternaryOperator.js
@@ -0,0 +1,30 @@
+/*
+
+	Перепишите if..else с использованием нескольких операторов '?'.
+	
+	let message;
+
+	if (login == 'Сотрудник') {
+		message = 'Привет';
+	} else if (login == 'Директор') {
+		message = 'Здравствуйте';
+	} else if (login == '') {
+		message = 'Нет логина';
+	} else {
+		message = '';
+	}
+
+*/
+
+let login = '';
+
+let message =
+  login == 'Сотрудник'
+    ? 'Привет'
+    : login == 'Директор'
+    ? 'Здравствуйте'
+    : login == ''
+    ? 'Нет логина'
+    : '';
+
+console.log(message);
