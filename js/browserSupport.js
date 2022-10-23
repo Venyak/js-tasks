e5ab8a848dcd5c70bd64c45a8796e96bd5e9eb21
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-23 10:54:10 +0500

    Create browserSupport.js

---
+++ js/browserSupport.js
@@ -0,0 +1,40 @@
+/*
+	Напишите if..else, соответствующий следующему switch:
+
+	switch (browser) {
+		case 'Edge':
+			alert( "You've got the Edge!" );
+			break;
+
+		case 'Chrome':
+		case 'Firefox':
+		case 'Safari':
+		case 'Opera':
+			alert( 'Okay we support these browsers too' );
+			break;
+
+		default:
+			alert( 'We hope that this page looks ok!' );
+	}
+*/
+
+const browserSupport = (browser) => {
+  let answer;
+  if (browser.toLowerCase() === 'edge') {
+    answer = "You've got the Edge!";
+    return answer;
+  } else if (
+    browser.toLowerCase() === 'chrome' ||
+    browser.toLowerCase() === 'firefox' ||
+    browser.toLowerCase() === 'safari' ||
+    browser.toLowerCase() === 'opera'
+  ) {
+    answer = 'Okay we support these browsers too';
+    return answer;
+  } else {
+    answer = 'We hope that this page looks ok!';
+    return answer;
+  }
+};
+
+console.log(browserSupport('opera'));
