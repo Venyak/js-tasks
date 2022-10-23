54af431d6ca5f7fc35402bc8a86f8cd882b34ba2
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-23 11:26:42 +0500

    Update allPrimeNumbers.js

--- js/allPrimeNumbers.js
+++ js/allPrimeNumbers.js
@@ -2,7 +2,7 @@
 	Напишите код, который выводит все простые числа из интервала от 2 до n.
 */
 
-const allPrimeNumbers = (n) => {
+const showPrimeNumbers = (n) => {
   nextPrime: for (let i = 2; i <= n; i++) {
     for (let j = 2; j < i; j++) {
       if (i % j == 0) continue nextPrime;
