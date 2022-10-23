085567fc08bd5866bd4eedd8df30387c099cbf0e
Лида Рудова <102348531+Venyak@users.noreply.github.com>
2022-10-23 10:46:02 +0500

    Create primeNumbers.js

---
+++ js/primeNumbers.js
@@ -0,0 +1,15 @@
+/*
+	Напишите код, который выводит все простые числа из интервала от 2 до n.
+*/
+
+const primeNumbers = (n) => {
+  nextPrime: for (let i = 2; i <= n; i++) {
+    for (let j = 2; j < i; j++) {
+      if (i % j == 0) continue nextPrime;
+    }
+
+    console.log(i);
+  }
+};
+
+primeNumbers(100);
